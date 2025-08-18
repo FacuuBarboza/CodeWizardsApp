5ead99ef8d8614b06c4e150032123cfe49f8a38e
FacuuBarboza <facu.mbarboza@gmail.com>
2025-08-17 22:01:16 -0300

    Se hicieron todos los cambios para que funciones los comentarios y se puedan eliminar. Se arregló index para que filtre correctamente. Se lograron cambios para que se vean las tarjetas

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from apps.accounts.models import User
 from apps.posts.models import PostsModel
 from django.utils import timezone
 from django.conf import settings
@@ -13,9 +14,17 @@ class CommentsModel(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField(max_length=200)
     created_at = models.DateTimeField(default=timezone.now)
+    likes_count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"Comentario de {self.user}"
 
+class CommentLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(CommentsModel, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user', 'comment')
 
 # Create your models here.
